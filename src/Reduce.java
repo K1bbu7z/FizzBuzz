cd6b64dcf394b5d978d22a04656a70b3135cc746
dengxues <K1bbu7z@gmail.com>
2024-09-09 21:11:01 -0400

    better style

--- src/Reduce.java
+++ src/Reduce.java
@@ -3,16 +3,25 @@ public class Reduce {
         int n = 100;
         int step = 0;
         boolean zero;
+
         while (zero = n != 0) {
+
             boolean divisibleBy2 = n % 2 == 0;
+
             if (divisibleBy2) {
+
                 n = n / 2;
                 step++;
+
             } else {
+
                 n--;
                 step++;
+
             }
         }
+
         System.out.println(step);
+
     }
 }
