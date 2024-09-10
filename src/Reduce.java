62ff9d5cda3874649869b686b98c510fef9af66c
dengxues <K1bbu7z@gmail.com>
2024-09-09 20:51:41 -0400

    New feature Reduce

---
+++ src/Reduce.java
@@ -0,0 +1,18 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int step = 0;
+        boolean zero;
+        while (zero = n != 0) {
+            boolean divisibleBy2 = n % 2 == 0;
+            if (divisibleBy2) {
+                n = n / 2;
+                step++;
+            } else {
+                n--;
+                step++;
+            }
+        }
+        System.out.println(step);
+    }
+}
